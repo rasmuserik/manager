03934cedac23fcbf512ab60c63247c6aa615cbe8
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-02-07 18:16:08 +0100

    content parsing

--- src/manager/core.clj
+++ src/manager/core.clj
@@ -4,6 +4,7 @@
   (:require
      [clj-yaml.core :as yaml]
      [manager.nginx :as nginx]
+     [clojure.pprint :as pprint]
      [manager.logger :as logger]))
 
 (defn createNginxConf []
@@ -11,6 +12,16 @@
 (createNginxConf)
 
 (logger/log "hello" :world {:foo :bar :baz :quux})
+
+(def data (yaml/parse-string (slurp "content.yml")))
+(def data (apply concat (apply concat (for [[section val] data]
+  (for [[subsection val] val]
+    (for [[id obj] val]
+    (conj obj [:section section] [:subsection subsection] [:id id])))))))
+(def data (sort-by #(:date %) data))
+data
+
+(pprint/pprint data)
 
 (defn -main
   [& args]
